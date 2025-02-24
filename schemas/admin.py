cc337f693c8979662becfbdef585e522f2a42693
vardges <arsenyanvardges@gmail.com>
2025-02-24 17:31:11 +0400

    Create admin.py

---
+++ schemas/admin.py
@@ -0,0 +1,19 @@
+class admin()
+  name:str
+  surname:str
+
+class book_room()
+  room_Name
+  start
+  end
+  capacity 
+  activity
+
+class delete_booking()
+  room_name
+  start 
+  end
+
+class delete_student()
+  student_name
+  
