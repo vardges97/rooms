10e65b6217fa418cebb3523aa34d79b0fd045d19
vardges <arsenyanvardges@gmail.com>
2025-02-24 17:19:16 +0400

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,17 @@
+class user()
+  name
+  surname 
+  email
+  role
+  group
+  secret code
+
+  def upload_dict(self):
+    return{
+      name
+      surname
+      email
+      role
+      group
+      secret code
+    }
